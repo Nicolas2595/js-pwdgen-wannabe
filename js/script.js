237ef033cf3fa84f5265fbd0f45f670cda72e241
Nicolas2595 <nmorelli10@gmail.com>
2021-04-26 15:41:15 +0200

    aggiunta var password

--- js/script.js
+++ js/script.js
@@ -20,6 +20,11 @@ console.log(coloreUtente);
 // Numero
 var numero = 21;
 
+// Password
+var password = "nicolasmorellinero21";
+console.log(password);
+
+
 // Scrive sulla pagina
 document.getElementById("password").innerHTML = nomeUtente + cognomeUtente + coloreUtente + numero;
 
